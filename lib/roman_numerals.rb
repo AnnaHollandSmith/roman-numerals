a5437c0d86b2114eaa06f035a2152024b0005749
Anna <anna.hollandsmith3@gmail.com>
2016-09-12 16:32:47 +0100

    extended the hash for the constant to include all symbols excluding subtractive notation"

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,6 +1,14 @@
 class RomanNumerals
 
-  ROMAN_NUMERALS = { 1 => "I", 5 => "V", 10 => "X" }
+  ROMAN_NUMERALS = {
+                  1 => "I",
+                  5 => "V",
+                  10 => "X",
+                  50 => "L",
+                  100 => "C",
+                  500 => "D",
+                  1000 => "M"
+                }
 
   def self.convert(n)
     ROMAN_NUMERALS[n]
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -12,7 +12,23 @@ describe RomanNumerals do
     end
 
     it "should convert 10 into X" do
-      expect(RomanNumerals.convert(10)).to eq("X") 
+      expect(RomanNumerals.convert(10)).to eq("X")
+    end
+
+    it "should convert 50 into L" do
+      expect(RomanNumerals.convert(50)).to eq("L")
+    end
+
+    it "should convert 100 into C" do
+      expect(RomanNumerals.convert(100)).to eq("C")
+    end
+
+    it "should convert 500 into D" do
+      expect(RomanNumerals.convert(500)).to eq("D")
+    end
+
+    it "should convert 1000 into M" do
+      expect(RomanNumerals.convert(1000)).to eq("M")
     end
   end
 end
