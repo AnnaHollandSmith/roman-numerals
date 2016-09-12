691bb85e8c15c5df5cfb8e3891bb5f598a133378
Anna <anna.hollandsmith3@gmail.com>
2016-09-12 16:11:01 +0100

    added second test to ensure that the number 5 is converted to X"

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,5 +1,8 @@
 class RomanNumerals
+
   def self.convert(n)
+    return "V" if n == 5
     "I"
   end
+
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -2,8 +2,13 @@ require "roman_numerals"
 
 describe RomanNumerals do
   context "it converts a given number into a roman numeral" do
+
     it "should convert 1 into I" do
       expect(RomanNumerals.convert(1)).to eq("I")
+    end
+
+    it "should convert 5 into V" do
+      expect(RomanNumerals.convert(5)). to eq("V")
     end
   end
 end
